e52ca6637157141cb0e86025f0a7b7f06f4565e6
sesilvia02 <ssilvia@tentantly.com>
2018-11-17 15:50:32 -0500

    Added listing item with toggle able class that switches panels and items

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-//import { GoogleApiWrapper, InfoWindow, Map, Marker } from 'google-maps-react';
-
+import { GoogleApiWrapper, InfoWindow, Map, Marker } from 'google-maps-react';
 import logo from './logo.svg';
 import './App.css';
 import ActivityListing from './containers/ActivityListing';
@@ -39,6 +38,10 @@ class App extends Component {
      console.log('latitude', position.coords.latitude,
                  'longitude', position.coords.longitude);
                  thisItem.setState({position:position});
+                 const listmenu = document.getElementById('results');
+                  listmenu.style.display = 'block';
+                const welcome = document.getElementById('welcome');
+                welcome.style.display = 'none';
    },
   function error(error_message) {
   console.error('Your browser does not support navigation');
@@ -67,31 +70,34 @@ class App extends Component {
           <div className="App">
             <img src={logo} className="App-logo" alt="logo" />
             <div className="container">
-              <div className="panel panel--rh">
-                <h1>Are you Bored? Wondering what to do? You are the right place</h1>
-                <button name="FindMe"  className="btn btn-primary" onClick={(e) => this.handleClick(e)}>
-                        Find Me
-                </button>
-                <p>Or</p>
-                <input type="text" name="zipcode" placeholder="zipcode" onChange={this.handleChange} />
-                <input type="submit" value="Submit" />
-                <ActivityListing position={this.state.position} />
+
+
+            <div id="welcome">
+              <h1 className="title-salutations">Welcome</h1>
+                <div className="panel panel--rh panel--sm">
+                  <p>Are you Bored? Wondering what to do? You are the right place</p>
+                  <button name="FindMe"  className="btn btn-lg btn-primary btn--find" onClick={(e) => this.handleClick(e)}>Find Me!</button>
+                  <p className="or-text">Or</p>
+                  <div className="rh-form-group">
+                    <input className="form-control" type="text" name="zipcode" placeholder="zipcode" onChange={this.handleChange} />
+                    <input type="submit" value="Submit" className="rh-form-submit"/>
+                  </div>
+                </div>
               </div>
+
+
+
+              <div className="listing-results" id="results">
+                <ActivityListing position={this.state.position}/>
+              </div>
+
+
             </div>
           </div>
 
           ) }
         />
 
-        <Route
-          exact
-          path="/ActivityListing" component={ActivityListing}
-        />
-
-        <Route
-          exact
-          path="/LocationPage" component={LocationPage}
-        />
 
       </Switch>
 
--- src/containers/ActivityListing.js
+++ src/containers/ActivityListing.js
@@ -4,6 +4,11 @@ import '../assets/styles/activitylisting.css';
 class ActivityListing extends Component {
   constructor(props) {
       super(props);
+
+      this.state = {
+      		hideListing: true
+      };
+      
       // console.log('Inside act');
       console.log(8,props);
       //this.handleChange = this.handleChange.bind(this);
@@ -15,25 +20,17 @@ class ActivityListing extends Component {
       //console.log(this.state.position);
       var lat= this.props.position && this.props.position.coords && this.props.position.coords.latitude
 	    return(
-	        <section className="">
-	        	<div className="container">
-	        		<div className="row">
-	        			<div className="col-xs-12 col-sm-4">
-	        				<section className="listing-item">
-	        					<div className="listing-item__img"></div>
-	        					<div className="listing-item__details">
-	        						<h2 className="listing-item__title text-center">Title</h2>
-	        						<div className="listing-item__desc text-center">{lat}</div>
-	        					</div>
-	        					<footer className="listing-item__footer">
-	        						<a href="#" className="listing-item__action listing-item__action--dismiss">Pass</a>
-	        						<a href="#URL" className="listing-item__action listing-item__action--love">Lets Do It!</a>
-	        					</footer>
-	        				</section>
-	        			</div>
-	        		</div>
-	        	</div>
-	        </section>
+		<section className="listing-item">
+			<div className="listing-item__img"></div>
+			<div className="listing-item__details">
+				<h2 className="listing-item__title text-center">Title</h2>
+				<div className="listing-item__desc text-center">{lat}</div>
+			</div>
+			<footer className="listing-item__footer">
+				<a href="#" className="listing-item__action listing-item__action--dismiss">Pass</a>
+				<a href="#URL" className="listing-item__action listing-item__action--love">Lets Do It!</a>
+			</footer>
+		</section>   			
         )
     }
 }
