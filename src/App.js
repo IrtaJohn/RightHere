cbc46168210721a5f0d735b7f1d7bb32e22cd2e3
Soundharya <ksoundharya@gmail.com>
2018-11-17 11:26:07 -0500

    added some more changes

--- src/App.js
+++ src/App.js
@@ -3,10 +3,35 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+     super(props);
+     this.state = {value: ''};
+     this.handleChange = this.handleChange.bind(this);
+   }
+
+
+  handleClick() {
+   console.log('this is:', this.state);
+ }
+
+ handleChange(event) {
+   this.setState({value: event.target.value});
+   console.log(event.target.value);
+ }
   render() {
     return (
       <div className="App">
-        Test
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Are you Bored? Wondering what to do? You are the right place
+          </p>
+          <button name="FindMe"  className="button" onClick={(e) => this.handleClick(e)}>
+                  Find Me
+          </button>
+        <p> Or</p>
+          <input type="text" name="zipcode" placeholder="zipcode" onChange={this.handleChange} />
+          <input type="submit" value="Submit" />
+
       </div>
     );
   }
