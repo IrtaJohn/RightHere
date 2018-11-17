ac0bb1f458a7fa24e72519015b9c871609c7469e
Irta John <irta.john@ten24web.com>
2018-11-17 15:52:09 +0000

    containers and component folders

---
+++ src/containers/MapContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"; 
+import { Link } from 'react-router';
+
+class MapContainer extends Component {
+    constructor(props){
+        
+    }
+    
+    render(){
+        
+    }
+    
+}
