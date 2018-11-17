95847dc0b788b5ed5d4e753c38fb3126a38a1fc0
sesilvia02 <ssilvia@tentantly.com>
2018-11-17 11:29:43 -0500

    Added bootstrap 4 as new dependceny and new file for Activity listing

---
+++ src/containers/ActivityListing.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react"; 
+import { Link } from 'react-router';
+class ActivityListing extends Component {
+    
+    render(){ 
+	    
+	    return(
+	        <section class="">
+	        	<div class="container">
+	        		
+	        	</div>
+	        </section>
+        )
+    }
+
+    
+    
+}
